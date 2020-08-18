af1de7cf25af05bcdec0384f2067dc228a29ec54
xuyaozu <2589231996@qq.com>
2020-08-18 15:24:53 +0800

    feat:add router v1/guide

---
+++ pages/v1/guide.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-container fluid>
+    <NavHeader />
+  </b-container>
+</template>
+
+<script>
+import NavHeader from '~/components/common/NavHeader'
+export default {
+  components: {
+    NavHeader
+  }
+}
+</script>
