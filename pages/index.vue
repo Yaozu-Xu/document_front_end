3729a4fec36e6a7b5b0676b825dbd0a6c303120e
xxiaozhi <1347505328@qq.com>
2020-08-19 19:29:40 +0800

    Refactor: Delete initial content of nuxtjs project

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,11 @@
 <template>
-  <div class="container">
-    <div>
-      <div>
-        <h2>Example heading <b-badge>New</b-badge></h2>
-        <h3>Example heading <b-badge>New</b-badge></h3>
-        <h4>Example heading <b-badge>New</b-badge></h4>
-        <h5>Example heading <b-badge>New</b-badge></h5>
-        <h6>Example heading <b-badge>New</b-badge></h6>
-        <input type="text" data-test="inputtest">
-      </div>
-      <Logo />
-      <h1 class="title">
-        muti_language
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <b-container fluid="sm">
+      100% wide until small breakpoint
+    </b-container>
+    <b-container fluid="md">
+      100% wide until medium breakpoint
+    </b-container>
   </div>
 </template>
 
@@ -40,42 +14,5 @@ export default {}
 </script>
 
 <style lang="scss">
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
