4e17a4d0d59d4059a96c959ab374ec6089b8b973
xxiaozhi <1347505328@qq.com>
2020-08-19 19:29:40 +0800

    Refactor: Delete initial content of nuxtjs project

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <navg />
     <Nuxt />
   </div>
 </template>
@@ -31,28 +32,8 @@ html {
   color: #3b8070;
   text-decoration: none;
   padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
+
 <script>
 import navbar from "../components/public/nav.vue";
 
