6674a7af3cab8706af3da1f942344c2f977b98e1
xuyaozu <2589231996@qq.com>
2020-08-23 18:10:53 +0800

    Feature: Add common component DocumentContainer

--- pages/v1/guide.vue
+++ pages/v1/guide.vue
@@ -1,11 +1,14 @@
 <template>
-  <b-container>
+  <b-container class="d-flex">
+    <!-- <b-container class="d-flex w-25 h-100">
+      aaa
+    </b-container> -->
     <NavHeader />
   </b-container>
 </template>
 
 <script>
-import NavHeader from '~/components/common/NavHeader'
+import NavHeader from '~/components/common/DocumentConatiner'
 export default {
   components: {
     NavHeader
