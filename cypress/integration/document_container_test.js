368732b4e240f9247171a6903196ea7dcbebd0a4
xuyaozu <2589231996@qq.com>
2020-08-26 16:37:47 +0800

    Fix: update the testing url

--- cypress/integration/document_container_test.js
+++ cypress/integration/document_container_test.js
@@ -1,7 +1,7 @@
 describe('DocumentContainer Test', () => {
   it('click h2 scrolls successfully', () => {
     cy
-      .visit('localhost:3000/v1/guide')
+      .visit('http://localhost:3000/guide')
       .get('#t4')
       .find('a')
       .click()
