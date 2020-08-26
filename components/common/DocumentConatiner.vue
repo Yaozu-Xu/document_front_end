8b35b6543046c15e076a28eecc6bf9c85153728d
xuyaozu <2589231996@qq.com>
2020-08-26 08:20:27 +0800

    Fix: Fix the naming class of block elemnets

--- components/common/DocumentConatiner.vue
+++ components/common/DocumentConatiner.vue
@@ -1,6 +1,7 @@
 <template>
-  <b-container fluid class="mt-5 mb-5 d-flex flex-column document-container">
-    <b-container class="items mb-5">
+  <b-container fluid class="mt-5 mb-5 d-flex flex-column document">
+    <b-container class="document__content mb-5">
+      <!-- The id and href are rendered by backend apis, which is correspond with the content of leftsidebar -->
       <h2 id="t1" class="pb-3 mb-4">
         <a href="#t1">Vue.js是什么</a>
       </h2>
@@ -8,7 +9,7 @@
       <p>Vue (读音 /vjuː/，类似于 view) 是一套用于构建用户界面的渐进式框架。与其它大型框架不同的是，Vue 被设计为可以自底向上逐层应用。Vue 的核心库只关注视图层，不仅易于上手，还便于与第三方库或既有项目整合。另一方面，当与现代化的工具链以及各种支持类库结合使用时，Vue 也完全能够为复杂的单页应用提供驱动。</p>
       <p>Vue (读音 /vjuː/，类似于 view) 是一套用于构建用户界面的渐进式框架。与其它大型框架不同的是，Vue 被设计为可以自底向上逐层应用。Vue 的核心库只关注视图层，不仅易于上手，还便于与第三方库或既有项目整合。另一方面，当与现代化的工具链以及各种支持类库结合使用时，Vue 也完全能够为复杂的单页应用提供驱动。</p>
     </b-container>
-    <b-container class="items mb-4">
+    <b-container class="document__content mb-4">
       <h2 id="t2" class="pb-3 mb-4">
         <a href="#t2">Vue.js是什么</a>
       </h2>
@@ -16,7 +17,7 @@
       <p>Vue (读音 /vjuː/，类似于 view) 是一套用于构建用户界面的渐进式框架。与其它大型框架不同的是，Vue 被设计为可以自底向上逐层应用。Vue 的核心库只关注视图层，不仅易于上手，还便于与第三方库或既有项目整合。另一方面，当与现代化的工具链以及各种支持类库结合使用时，Vue 也完全能够为复杂的单页应用提供驱动。</p>
       <p>Vue (读音 /vjuː/，类似于 view) 是一套用于构建用户界面的渐进式框架。与其它大型框架不同的是，Vue 被设计为可以自底向上逐层应用。Vue 的核心库只关注视图层，不仅易于上手，还便于与第三方库或既有项目整合。另一方面，当与现代化的工具链以及各种支持类库结合使用时，Vue 也完全能够为复杂的单页应用提供驱动。</p>
     </b-container>
-    <b-container class="items mb-4">
+    <b-container class="document__content mb-4">
       <h2 id="t3" class="pb-3 mb-4">
         <a href="#t3">Vue.js是什么</a>
       </h2>
@@ -24,7 +25,7 @@
       <p>Vue (读音 /vjuː/，类似于 view) 是一套用于构建用户界面的渐进式框架。与其它大型框架不同的是，Vue 被设计为可以自底向上逐层应用。Vue 的核心库只关注视图层，不仅易于上手，还便于与第三方库或既有项目整合。另一方面，当与现代化的工具链以及各种支持类库结合使用时，Vue 也完全能够为复杂的单页应用提供驱动。</p>
       <p>Vue (读音 /vjuː/，类似于 view) 是一套用于构建用户界面的渐进式框架。与其它大型框架不同的是，Vue 被设计为可以自底向上逐层应用。Vue 的核心库只关注视图层，不仅易于上手，还便于与第三方库或既有项目整合。另一方面，当与现代化的工具链以及各种支持类库结合使用时，Vue 也完全能够为复杂的单页应用提供驱动。</p>
     </b-container>
-    <b-container class="items mb-4">
+    <b-container class="document__content mb-4">
       <h2 id="t4" class="pb-3 mb-4">
         <a href="#t4">Vue.js是什么</a>
       </h2>
