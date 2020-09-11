14fecfadee75afdb24f1fb15216d75f84a4875f2
handsome0hell <handsome0hell@gmail.com>
2020-09-11 15:44:24 +0800

    Config: Improve testing framework
    Ignore coverage for test files.
    Combine coverage from multiple tests.

--- src/tests/support/index.js
+++ src/tests/support/index.js
@@ -1,3 +1,13 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 // We're runing tests, so importing devDependencies is fine
 import "@cypress/code-coverage/support";
+
+afterEach(() => {
+  cy.window().then((win) => {
+    /* eslint-disable-next-line no-underscore-dangle -- __coverage__ is defined by istanbul */
+    const coverage = win.__coverage__;
+    if (coverage) {
+      cy.task("combineCoverage", JSON.stringify(coverage));
+    }
+  });
+});
