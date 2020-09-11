9cfbea42482c21ce60a130e91385a6e4a26cccd1
handsome0hell <handsome0hell@gmail.com>
2020-09-11 15:40:34 +0800

    Config: Allow single quote to avoid escape in ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,7 @@ module.exports = {
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
+    quotes: ["error", "double", { avoidEscape: true }],
     semi: ["error", "always"],
   },
   settings: {
