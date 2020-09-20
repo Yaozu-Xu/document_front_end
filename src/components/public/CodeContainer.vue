2fc52c610665a65e965a0c34acfb1d693d452094
xuyaozu <2589231996@qq.com>
2020-09-20 20:08:02 +0100

    Feature: Create public component CodeContainer to display code

---
+++ src/components/public/CodeContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="code-container">
+    <pre class="p-3 p-md-4">{{ code }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    code: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+pre {
+  background-color: #f8f8f8;
+  white-space: pre;
+  overflow: auto;
+}
+</style>
